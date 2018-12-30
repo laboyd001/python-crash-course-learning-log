e8f8776bead0446d04f7cfbd90c747de9194e86c
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-30 15:49:39 -0600

    added heroku to allowed hosts

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -146,9 +146,12 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     #Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-    #Allow all host headers
-    ALLOWED_HOSTS = ['*']
+    #Allow only Heroku to host the project
+    ALLOWED_HOSTS = ['lesleys-learning-log.herokuapp.com']
 
+    DEBUG = False
+
+    
     #Static asset config
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
